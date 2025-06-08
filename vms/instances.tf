38d60e0c58811ce4ecea15094988d7a1ef03d05b
Kevin Lapagna <klapagna@redhat.com>
2025-06-08 19:41:33 +0000

    feat: Add VM per REQ555555

--- vms/instances.tf
+++ vms/instances.tf
@@ -60,3 +60,24 @@ module "loyal_midge_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - loyal_midge
+# BEGIN ANSIBLE MANAGED BLOCK - next_squid
+
+# next squid VM Instance - Added by Ansible
+module "next_squid_vm" {
+  source = "./modules/ec2-instance"
+
+  # Required parameters
+  instance_name = "next-squid-VM"
+  instance_type = var.instance_type
+
+  # Default parameters (can be overridden as needed)
+  ami_id             = var.ami_id
+  key_name           = aws_key_pair.vm_auth.key_name
+  subnet_id          = aws_subnet.public.id
+  security_group_ids = [aws_security_group.allow_ssh.id]
+  tags = {
+    Environment = "Development"
+    CreatedBy   = "Ansible"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - next_squid
