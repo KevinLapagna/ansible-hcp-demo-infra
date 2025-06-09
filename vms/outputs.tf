b2d9a27a51bf719ea7a1b84fcf6c6e04730176b5
Kevin Lapagna <klapagna@redhat.com>
2025-06-09 17:03:53 +0200

    feat: Remove unused VM instances and outputs for cleanup

--- vms/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance."
-  value       = module.fedora_vm.instance_public_ip
-}
-
-output "instance_id" {
-  description = "ID of the EC2 instance."
-  value       = module.fedora_vm.instance_id
-}
