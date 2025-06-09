aaf1961c774c9cd80f894f52826cba9864211dad
Kevin Lapagna <klapagna@redhat.com>
2025-06-09 09:30:10 +0000

    feat: Add VM per REQ46429304

--- vms/instances.tf
+++ vms/instances.tf
@@ -81,3 +81,24 @@ module "next_squid_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - next_squid
+# BEGIN ANSIBLE MANAGED BLOCK - bold_lab
+
+# bold lab VM Instance - Added by Ansible
+module "bold_lab_vm" {
+  source = "./modules/ec2-instance"
+
+  # Required parameters
+  instance_name = "bold-lab-VM"
+  instance_type = var.instance_type
+
+  # Default parameters (can be overridden as needed)
+  ami_id             = var.ami_id
+  key_name           = aws_key_pair.vm_auth.key_name
+  subnet_id          = aws_subnet.public.id
+  security_group_ids = [aws_security_group.allow_ssh.id]
+  tags = {
+    Environment = "Development"
+    CreatedBy   = "Ansible"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - bold_lab
