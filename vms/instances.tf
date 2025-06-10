26dd0209a2346f094223f48decda2718a240b527
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-10 08:02:21 +0200

    feat: Add VM per REQKEV1234
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQKEV1234.

--- vms/instances.tf
+++ vms/instances.tf
@@ -18,3 +18,24 @@
 #   #   Role        = "WebServer"
 #   # }
 # }
+# BEGIN ANSIBLE MANAGED BLOCK - united_doe
+
+# united doe VM Instance - Added by Ansible
+module "united_doe_vm" {
+  source = "./modules/ec2-instance"
+
+  # Required parameters
+  instance_name = "united-doe-VM"
+  instance_type = var.instance_type
+
+  # Default parameters (can be overridden as needed)
+  ami_id             = var.ami_id
+  key_name           = aws_key_pair.vm_auth.key_name
+  subnet_id          = aws_subnet.public.id
+  security_group_ids = [aws_security_group.allow_ssh.id]
+  tags = {
+    Environment = "Development"
+    CreatedBy   = "Ansible"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - united_doe
