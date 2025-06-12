a3b871c7532223162f66b3c650fa44cb7c80cadf
Kevin Lapagna <klapagna@redhat.com>
2025-06-12 11:30:51 +0000

    feat: Add VM per REQ666907611

--- vms/instances.tf
+++ vms/instances.tf
@@ -54,3 +54,22 @@ resource "aws_instance" "eager_coyote_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - eager_coyote
+# BEGIN ANSIBLE MANAGED BLOCK - clean_weasel
+resource "aws_instance" "clean_weasel_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a98b5f222a0d2396"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "clean-weasel-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - clean_weasel
