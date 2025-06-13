7b10949d29e2ddf68269194d6cf3f3ae2a0eb895
Kevin Lapagna <klapagna@redhat.com>
2025-06-13 15:01:08 +0000

    feat: Add VM per REQ439471020

--- vms/instances.tf
+++ vms/instances.tf
@@ -17,3 +17,22 @@ resource "aws_instance" "decent_locust_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - decent_locust
+# BEGIN ANSIBLE MANAGED BLOCK - fair_moose
+resource "aws_instance" "fair_moose_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a98b5f222a0d2396"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "fair-moose-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - fair_moose
