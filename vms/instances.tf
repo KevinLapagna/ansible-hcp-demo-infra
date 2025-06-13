8bf03e9e578a7f5df05be03f8b3ce21bf6f1b90b
Kevin Lapagna <klapagna@redhat.com>
2025-06-13 19:17:28 +0000

    feat: Add VM per REQ895569176

--- vms/instances.tf
+++ vms/instances.tf
@@ -36,3 +36,22 @@ resource "aws_instance" "vast_monkey_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - vast_monkey
+# BEGIN ANSIBLE MANAGED BLOCK - novel_dory
+resource "aws_instance" "novel_dory_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a98b5f222a0d2396"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "novel-dory-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - novel_dory
