32c11305008827fd28fab502942c02252209e5b7
Kevin Lapagna <klapagna@redhat.com>
2025-06-13 21:48:02 +0000

    feat: Add VM per REQ228159525

--- vms/instances.tf
+++ vms/instances.tf
@@ -74,3 +74,22 @@ resource "aws_instance" "true_cougar_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - true_cougar
+# BEGIN ANSIBLE MANAGED BLOCK - set_stork
+resource "aws_instance" "set_stork_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "var.ami_id"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "set-stork-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - set_stork
