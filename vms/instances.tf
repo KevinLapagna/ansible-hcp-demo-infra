73ba46346ba3e8a67567f1a7801913ad706a77e8
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-13 21:19:55 +0200

    feat: Add VM per REQ50031636
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQ50031636.

--- vms/instances.tf
+++ vms/instances.tf
@@ -55,3 +55,22 @@ resource "aws_instance" "novel_dory_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - novel_dory
+# BEGIN ANSIBLE MANAGED BLOCK - true_cougar
+resource "aws_instance" "true_cougar_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a98b5f222a0d2396"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "true-cougar-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - true_cougar
