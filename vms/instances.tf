a3a801064514a65a3ebcf0e2c717ff23687f341a
Kevin Lapagna <klapagna@redhat.com>
2025-06-13 18:48:39 +0000

    feat: Add VM per REQ606484510

--- vms/instances.tf
+++ vms/instances.tf
@@ -36,3 +36,22 @@ resource "aws_instance" "fair_moose_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - fair_moose
+# BEGIN ANSIBLE MANAGED BLOCK - needed_raptor
+resource "aws_instance" "needed_raptor_vm" {
+  provider = aws.eu_west_1
+
+  ami           = "ami-05fd4b86367f3a4b9"
+  instance_type = var.instance_type
+  key_name      = module.eu_west_1[0].key_pair_name
+  subnet_id     = module.eu_west_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_west_1[0].security_group_id]
+
+  tags = {
+    Name        = "needed-raptor-VM-eu-west-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-west-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - needed_raptor
