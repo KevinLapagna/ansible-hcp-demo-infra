7480c9eaa8cc36b0b34ca7649e6462024627413d
Kevin Lapagna <klapagna@redhat.com>
2025-06-13 22:30:28 +0000

    feat: Add VM per REQ705999057

--- vms/instances.tf
+++ vms/instances.tf
@@ -74,3 +74,22 @@ resource "aws_instance" "true_cougar_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - true_cougar
+# BEGIN ANSIBLE MANAGED BLOCK - calm_fowl
+resource "aws_instance" "calm_fowl_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "var.ami_id"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "calm-fowl-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - calm_fowl
