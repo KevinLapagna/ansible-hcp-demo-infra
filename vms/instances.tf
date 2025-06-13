ab0e176e8fd5efaadabf8761722e1c72e3c7ce51
Kevin Lapagna <klapagna@redhat.com>
2025-06-13 12:44:57 +0000

    feat: Add VM per REQ697235990

---
+++ vms/instances.tf
@@ -0,0 +1,19 @@
+# BEGIN ANSIBLE MANAGED BLOCK - decent_locust
+resource "aws_instance" "decent_locust_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "Fedora CoreOS 42"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "decent-locust-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - decent_locust
