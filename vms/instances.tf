afa7b5e90d950ee8239e42d9b44d1dddb57dd6ab
Kevin Lapagna <klapagna@redhat.com>
2025-06-17 15:28:49 +0000

    feat: Add VM per REQ559050000

--- vms/instances.tf
+++ vms/instances.tf
@@ -112,3 +112,22 @@ resource "aws_instance" "deep_roughy_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - deep_roughy
+# BEGIN ANSIBLE MANAGED BLOCK - witty_leech
+resource "aws_instance" "witty_leech_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a98b5f222a0d2396"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "witty-leech-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - witty_leech
