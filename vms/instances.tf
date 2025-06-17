e88c1042155ccac82856f8d5882a47218f80ab54
Kevin Lapagna <klapagna@redhat.com>
2025-06-17 15:02:09 +0000

    feat: Add VM per REQ110169880

--- vms/instances.tf
+++ vms/instances.tf
@@ -93,3 +93,22 @@ resource "aws_instance" "noted_osprey_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - noted_osprey
+# BEGIN ANSIBLE MANAGED BLOCK - ruling_racer
+resource "aws_instance" "ruling_racer_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a98b5f222a0d2396"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "ruling-racer-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - ruling_racer
