8b3c0e37f9040dcbcb83d7dc07f6e2aeffaecada
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-17 17:14:37 +0200

    feat: Add VM per REQ500365820
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQ500365820.

--- vms/instances.tf
+++ vms/instances.tf
@@ -93,3 +93,22 @@ resource "aws_instance" "ruling_racer_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - ruling_racer
+# BEGIN ANSIBLE MANAGED BLOCK - deep_roughy
+resource "aws_instance" "deep_roughy_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a98b5f222a0d2396"
+  instance_type = var.instance_type
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "deep-roughy-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - deep_roughy
