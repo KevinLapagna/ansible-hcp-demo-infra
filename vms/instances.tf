1e7dd5bba03ac1b3f9088865d0ff1518b272b093
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-20 17:45:56 +0200

    feat: Add VM per REQ86338497
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQ86338497.

--- vms/instances.tf
+++ vms/instances.tf
@@ -179,3 +179,31 @@ resource "aws_instance" "witty_leech_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - witty_leech
+# BEGIN ANSIBLE MANAGED BLOCK - social_oyster
+resource "aws_instance" "social_oyster_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-09f31a65dd0bdca78"  # Microsoft Windows Server 2022 Base
+  # ami         = "ami-0a98b5f222a0d2396"          
+  instance_type = "t3.medium"  # Windows requires more resources than t2.micro
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  # Enable detailed monitoring for Windows instances
+  monitoring = true
+
+  # User data script to configure WinRM with certificate authentication
+  user_data = base64encode(templatefile("${path.module}/windows-winrm-basic.ps1", {
+    certificate_content = file("${path.module}/aap-client-certificate.crt")
+  }))
+
+  tags = {
+    Name        = "social-oyster-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Windows"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - social_oyster
