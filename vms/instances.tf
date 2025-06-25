ec0c831cc9f8848870b6d29850ea2d9a4e692a80
Kevin Lapagna <klapagna@redhat.com>
2025-06-25 12:17:39 +0000

    feat: Add VM per REQ522964551

--- vms/instances.tf
+++ vms/instances.tf
@@ -159,3 +159,24 @@ resource "aws_instance" "ace_crab_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - ace_crab
+# BEGIN ANSIBLE MANAGED BLOCK - decent_cub
+resource "aws_instance" "decent_cub_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a8e2e55de614b2b1"
+  instance_type = "t3.medium"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "decent-cub-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Linux"
+    RequestID   = "REQ522964551"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - decent_cub
