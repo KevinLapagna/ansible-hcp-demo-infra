d815d9a206664934771088b8731a698fbd38edea
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-25 21:43:37 +0200

    feat: Add VM per REQ525324972
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQ525324972.

--- vms/instances.tf
+++ vms/instances.tf
@@ -251,3 +251,24 @@ resource "aws_instance" "steady_mutt_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - steady_mutt
+# BEGIN ANSIBLE MANAGED BLOCK - live_civet
+resource "aws_instance" "live_civet_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a8e2e55de614b2b1"
+  instance_type = "t3.medium"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "live-civet-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Linux"
+    RequestID   = "REQ525324972"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - live_civet
