13b2467049d9eaefbb38e3bf09b254fd4fedb53d
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-25 14:23:26 +0200

    feat: Add VM per REQ232780051
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQ232780051.

--- vms/instances.tf
+++ vms/instances.tf
@@ -180,3 +180,32 @@ resource "aws_instance" "decent_cub_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - decent_cub
+# BEGIN ANSIBLE MANAGED BLOCK - sought_cod
+resource "aws_instance" "sought_cod_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-09f31a65dd0bdca78"
+  instance_type = "t2.micro"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].windows_winrm_security_group_id]
+
+  # Enable detailed monitoring for Windows instances
+  monitoring = true
+
+  # User data script to configure WinRM with certificate authentication
+  user_data = base64encode(templatefile("${path.module}/windows-winrm-basic.ps1", {
+    certificate_content = file("${path.module}/aap-client-certificate.crt")
+  }))
+
+  tags = {
+    Name        = "sought-cod-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Windows"
+    RequestID   = "REQ232780051"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - sought_cod
