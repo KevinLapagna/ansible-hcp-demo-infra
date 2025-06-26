ed6f467e1a3cf2a023088ce83e7d303e1694e8ce
Kevin Lapagna <klapagna@redhat.com>
2025-06-26 15:46:52 +0000

    feat: Add VM per REQ413397188

--- vms/instances.tf
+++ vms/instances.tf
@@ -48,3 +48,24 @@ resource "aws_instance" "bold_insect_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - bold_insect
+# BEGIN ANSIBLE MANAGED BLOCK - fast_molly
+resource "aws_instance" "fast_molly_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a8e2e55de614b2b1"
+  instance_type = "t2.micro"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "fast-molly-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Linux"
+    RequestID   = "REQ413397188"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - fast_molly
