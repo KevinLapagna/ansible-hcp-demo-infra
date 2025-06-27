9f12c81ae595b474017d5f8f6c3a62ca88ccaf25
Kevin Lapagna <klapagna@redhat.com>
2025-06-27 15:09:39 +0000

    feat: Add VM per REQ792103635

--- vms/instances.tf
+++ vms/instances.tf
@@ -48,3 +48,24 @@ resource "aws_instance" "in_magpie_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - in_magpie
+# BEGIN ANSIBLE MANAGED BLOCK - social_cow
+resource "aws_instance" "social_cow_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a8e2e55de614b2b1"
+  instance_type = "t2.micro"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "social-cow-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Linux"
+    RequestID   = "REQ792103635"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - social_cow
