797885d7389cd2fd871a4bb12fb59889942fe648
Kevin Lapagna <klapagna@redhat.com>
2025-06-27 13:35:59 +0000

    feat: Remove VM steady_wombat per REQ453546096

--- vms/instances.tf
+++ vms/instances.tf
@@ -1,24 +1,3 @@
-# BEGIN ANSIBLE MANAGED BLOCK - steady_wombat
-resource "aws_instance" "steady_wombat_vm" {
-  provider = aws.eu_central_1
-
-  ami           = "ami-0a8e2e55de614b2b1"
-  instance_type = "t3.medium"
-  key_name      = module.eu_central_1[0].key_pair_name
-  subnet_id     = module.eu_central_1[0].subnet_id
-
-  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
-
-  tags = {
-    Name        = "steady-wombat-VM-eu-central-1"
-    Environment = "Development"
-    CreatedBy   = "AAP"
-    Region      = "eu-central-1"
-    OsType      = "Linux"
-    RequestID   = "REQ376118538"
-  }
-}
-# END ANSIBLE MANAGED BLOCK - steady_wombat
 # BEGIN ANSIBLE MANAGED BLOCK - alive_hawk
 resource "aws_instance" "alive_hawk_vm" {
   provider = aws.eu_central_1
