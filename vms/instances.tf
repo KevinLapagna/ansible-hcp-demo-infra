9007dc613e3df90764f9a288f117e635cef6aac0
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-27 17:05:56 +0200

    feat: Add VM per REQ841004789
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQ841004789.

--- vms/instances.tf
+++ vms/instances.tf
@@ -19,3 +19,32 @@ resource "aws_instance" "alert_osprey_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - alert_osprey
+# BEGIN ANSIBLE MANAGED BLOCK - in_magpie
+resource "aws_instance" "in_magpie_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-09f31a65dd0bdca78"
+  instance_type = "t3.medium"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].windows_winrm_security_group_id]
+
+  # Enable detailed monitoring for Windows instances
+  monitoring = true
+
+  # User data script to configure WinRM with certificate authentication
+  user_data = base64encode(templatefile("${path.module}/windows-winrm-basic.ps1", {
+    certificate_content = file("${path.module}/aap-client-certificate.crt")
+  }))
+
+  tags = {
+    Name        = "in-magpie-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Windows"
+    RequestID   = "REQ841004789"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - in_magpie
