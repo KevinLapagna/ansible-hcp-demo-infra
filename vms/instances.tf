e7443afd3caa189dce11084672df0aa81e8789ed
Kevin Lapagna <klapagna@redhat.com>
2025-06-27 12:59:30 +0000

    feat: Add VM per REQ769876084

---
+++ vms/instances.tf
@@ -0,0 +1,21 @@
+# BEGIN ANSIBLE MANAGED BLOCK - flying_teal
+resource "aws_instance" "flying_teal_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a8e2e55de614b2b1"
+  instance_type = "t2.micro"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "flying-teal-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Linux"
+    RequestID   = "REQ769876084"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - flying_teal
