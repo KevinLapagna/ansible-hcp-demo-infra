3b0018944a689ffaafbd4a5f4c3ea20984ad472a
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-27 13:49:24 +0200

    feat: Add VM per REQ756382901
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQ756382901.

--- vms/instances.tf
+++ vms/instances.tf
@@ -119,3 +119,32 @@ resource "aws_instance" "guided_sawfly_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - guided_sawfly
+# BEGIN ANSIBLE MANAGED BLOCK - fun_fish
+resource "aws_instance" "fun_fish_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-09f31a65dd0bdca78"
+  instance_type = "t3.medium"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].windows_winrm_security_group_id]
+
+  # Enable detailed monitoring for Windows instances
+  monitoring = true
+
+  # User data script to configure WinRM with certificate authentication
+  user_data = base64encode(templatefile("${path.module}/windows-winrm-basic.ps1", {
+    certificate_content = file("${path.module}/aap-client-certificate.crt")
+  }))
+
+  tags = {
+    Name        = "fun-fish-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Windows"
+    RequestID   = "REQ756382901"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - fun_fish
