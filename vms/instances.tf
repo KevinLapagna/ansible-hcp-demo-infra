fdea0c270d540f130c095dd94e01f3356a1c8d4d
Kevin Lapagna <24940828+KevinLapagna@users.noreply.github.com>
2025-06-27 15:05:28 +0200

    feat: Add VM per REQ376118538
    This pull request was automatically created by an Ansible playbook to add a new EC2 instance for request REQ376118538.

---
+++ vms/instances.tf
@@ -0,0 +1,21 @@
+# BEGIN ANSIBLE MANAGED BLOCK - steady_wombat
+resource "aws_instance" "steady_wombat_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a8e2e55de614b2b1"
+  instance_type = "t2.micro"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "steady-wombat-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Linux"
+    RequestID   = "REQ376118538"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - steady_wombat
