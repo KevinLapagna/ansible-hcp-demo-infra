0e8fc0eea946315713bf42d595718af9db1d9302
Kevin Lapagna <klapagna@redhat.com>
2026-01-08 10:50:14 +0000

    feat: Add VM per 123213213

--- vms/instances.tf
+++ vms/instances.tf
@@ -19,3 +19,24 @@ resource "aws_instance" "kevs01_vm" {
   }
 }
 # END ANSIBLE MANAGED BLOCK - kevs01
+# BEGIN ANSIBLE MANAGED BLOCK - hip_swan
+resource "aws_instance" "hip_swan_vm" {
+  provider = aws.eu_central_1
+
+  ami           = "ami-0a8e2e55de614b2b1"
+  instance_type = "t2.micro"
+  key_name      = module.eu_central_1[0].key_pair_name
+  subnet_id     = module.eu_central_1[0].subnet_id
+
+  vpc_security_group_ids = [module.eu_central_1[0].security_group_id]
+
+  tags = {
+    Name        = "hip-swan-VM-eu-central-1"
+    Environment = "Development"
+    CreatedBy   = "AAP"
+    Region      = "eu-central-1"
+    OsType      = "Linux"
+    RequestID   = "123213213"
+  }
+}
+# END ANSIBLE MANAGED BLOCK - hip_swan
